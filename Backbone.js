13c888a6751beb3ff498c0afaee00c8346fbc866
Insoo Kim <42515875+Insookim0702@users.noreply.github.com>
2018-10-11 23:21:16 +0900

    Create Backbone.js

---
+++ Backbone.js
@@ -0,0 +1,19 @@
+## What is the 'BACKBONE.js'
+
+ - HTML5용 화면 디자인용 framework이다.
+ - backbone.js는 클라이언트 사이드 웹어플리케이션 개발에 MVC패턴 적용을 가능하게 해주는 javaScript framework들 중 하나이다.
+ 
+ - backbone.js를 사용하면 MVC패턴에 맞게 개발 가능하다. 코드의 역할을 나눠서 작성하여 프로그램의 구조를 체계화 할 수 있다는 뜻이다.
+ - Collection과 View가 연동되어 데이터에 변화가 생길 때 화면 UI를 갱신해주는 일을 backbone.js를 통해 구현할 수 있다. 
+ 
+ - backbone.js는 서버사이드와 연동관련 기능도 있어 데이터의 CRUD(저장,조회,Update, Delete)를 편리하게 구현할 수 있다.
+ - underscore.js의 유틸리티 라이브러리에 의존성이 있다.
+ - Jquery를 사용할 수 있다.
+ 
+ 
+ ```
+ defaults :{//기본적으로 가지고 있어야 할 변수값}
+ 
+ initialize :function(){//객체를 new로 생성하면 1번만 실행되는 function}
+ ```
+ 
